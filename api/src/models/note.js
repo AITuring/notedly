1365cf1da50712af25fd00c275d909310e094ff9
AITuring <shspku365@gmail.com>
2021-02-22 17:47:41 +0800

    增加用户查询和笔记收藏

--- api/src/models/note.js
+++ api/src/models/note.js
@@ -10,7 +10,17 @@ const noteSchema = new mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
             required: true
-        }
+        },
+        favoriteCount: {
+            type: Number,
+            default: 0
+        },
+        favoritedBy: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+            }
+        ]
     },
     {
         // 添加Date类型的createdAt和updatedAt字段
--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -103,5 +103,50 @@ module.exports = {
         }
         // 创建并返回JWT
         return jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    },
+    toggleFavorite: async (parent, {id}, { models, user })=> {
+        // 如果上下文没有用户，抛出AuthenticationError
+        if (!user) {
+            throw new AuthenticationError('Error signing in')
+        }
+        // 检查用户是否已经收藏了该篇笔记
+        let noteCheck = await models.Note.findById(id)
+        const hasUser = noteCheck.favoritedBy.indexOf(user.id)
+
+        // 如果用户已经在列表中，把用户从列表中删除，favoriteCount值-1
+        if(hasUser >= 0) {
+            return await models.Note.findByIdAndUpdate(
+                id,
+                {
+                    $pull: {
+                        favoritedBy: mongoose.Types.ObjectId(user.id)
+                    },
+                    $inc: {
+                        favoriteCount: -1
+                    }
+                },
+                {
+                    // 把new变为true，返回更新后的笔记
+                    new: true
+                }
+            )
+        } else {
+            // 用户不在列表中
+            // 把用户添加到列表中，favoriteCount值+1
+            return await models.Note.findByIdAndUpdate(
+                id,
+                {
+                    $push: {
+                        favoritedBy: mongoose.Types.ObjectId(user.id)
+                    },
+                    $inc: {
+                        favoriteCount:1
+                    }
+                },
+                {
+                    new: true
+                }
+            )
+        }
     }
 }
